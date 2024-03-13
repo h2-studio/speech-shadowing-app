98de2ea7f5217254310b6f97b5e17b316754ae55
WH <wadehuang36@gmail.com>
2024-03-13 11:16:56 -0400

    update build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import solidPlugin from "vite-plugin-solid";
-import tsconfigPaths from 'vite-tsconfig-paths'
+import tsconfigPaths from "vite-tsconfig-paths";
 // import devtools from 'solid-devtools/vite';
 
 export default defineConfig({
@@ -19,5 +19,7 @@ export default defineConfig({
   },
   build: {
     target: "esnext",
+    outDir: "../dist",
+    emptyOutDir: true,
   },
 });
